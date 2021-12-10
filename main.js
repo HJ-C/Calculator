df7b48d62b6380e9ca98ab463f51d24ac5c5bad5
HJ-C <swch56@naver.com>
2021-12-10 22:41:02 +0900

    Calculator

---
+++ main.js
@@ -0,0 +1,51 @@
+function reset(){
+    forms.output.value += "''"
+}
+function btn_1() {
+    forms.output.value += "1"
+}
+function btn_2() {
+    forms.output.value += "2"
+}
+function btn_3() {
+    forms.output.value += "3"
+}
+function btn_4() {
+    forms.output.value += "4"
+}
+function btn_5() {
+    forms.output.value += "5"
+}
+function btn_6() {
+    forms.output.value += "6"
+}
+function btn_7() {
+    forms.output.value += "7"
+}
+function btn_8() {
+    forms.output.value += "8"
+}
+function btn_9() {
+    forms.output.value += "9"
+}
+function btn_0() {
+    forms.output.value += "0"
+}
+function opr_div() {
+    forms.output.value += "/"
+}
+function opr_mul() {
+    forms.output.value += "*"
+}
+function opr_pl() {
+    forms.output.value += "+"
+}
+function opr_mi() {
+    forms.output.value += "-"
+}
+function opr_dot() {
+    forms.output.value += "."
+}
+function result() {
+    forms.output.value = eval(document.forms.output.value)
+}
